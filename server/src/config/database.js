9811b04dd41e6276521060389d167073b3e6aa53
Animesh404 <akjha6680@gmail.com>
2024-12-15 14:48:23 +0530

    test desployement setup

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,14 +1,18 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
+const urlDB = 'mysql://root:UxvtTIloGXGTAyZbAOkQMuFfpbdPIrFn@mysql.railway.internal:3306/railway'
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'wepray',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USER || 'root',
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME || 'wepray',
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+
+const pool = mysql.createPool(urlDB);
+
 
 module.exports = pool;
