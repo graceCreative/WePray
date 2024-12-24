07ec4c1c69c857e8218afa42c59c22e89eea58c5
Grace Creatives <sumit.web6@gmail.com>
2024-12-24 22:50:20 +0530

    Soon page error resolved

--- client/src/pages/ComingSoon.jsx
+++ client/src/pages/ComingSoon.jsx
@@ -7,7 +7,7 @@ const ComingSoon = () => {
     <div>
         <Navbar/>
 
-        <div className="container flex flex-col justify-center items-center ">
+        <div className="container soon-section flex flex-col justify-center items-center ">
             <div className="circle"></div>
             <img src="https://ai.flux-image.com/flux/501b08a4-6164-4de3-8c6e-8e29d59e311b.jpg" alt="" className='comingImg'/>
             <h1 className='comingText'>Coming Soon</h1>
